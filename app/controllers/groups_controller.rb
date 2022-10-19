7f42b8756e3a216e3150efb8f1fbcc5a7a6ee55c
Brian Wangora <brianwangora@gmail.com>
2022-10-19 10:02:02 +0100

    Group routes working on postman

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,58 @@
 class GroupsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    # GET /groups
     def index 
         groups = Group.all 
         render json: groups
     end
+
+    # GET /groups/:id
+    def show 
+        group = Group.find(params[:id])
+        render json: group 
+    end
+
+    # POST /groups
+    def create 
+        group = Group.create(group_params)
+        render json: group, status: :created
+    end
+
+    # PATCH /groups/:id
+    def update
+        group = Group.find(params[:id])
+        if group
+            group.update(update_params)
+            render json: group 
+        else
+            render json: { error: "Group not found" }, status: :not_found
+        end
+    end
+
+    # DELETE /groups/:id
+    def destroy 
+        group = Group.find(params[:id])
+        if group 
+            group.destroy
+            head :no_content
+        else
+            render json: { error: "Group not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Group not found" }, status: :not_found
+    end
+
+    def group_params
+        params.permit(:title)
+    end
+
+    def update_params
+        params.permit(:title)
+    end
+
 end
