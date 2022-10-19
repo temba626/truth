60e6dc93af95d4923aeca296cf1f1cfbea158c0a
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-10-19 12:08:52 +0300

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,41 @@
 class UsersController < ApplicationController
-    def index 
-        users = User.all 
-        render json: users 
+    
+    def index
+        User=User.all
+        render json: User, status: :ok
+    end
+    
+    def show
+        User=User.find(params[:id])
+        if User
+            render json: User
+        else
+            render json: { error: "User not found" }, status: :not_found
+        end
+        
+    end
+    def create
+        User =User.create!(parameters)
+        render json: User, status: :created
+        
+    end
+  
+    def update
+        User=User.find(params[:id])
+        User.update(number: params(:number))
+        if User
+            render json: User
+        else
+            render json: { error: "User not found" }, status: :not_found
+        end
+    end
+    def destroy
+        User=User.find(params[:id])
+        User.destroy
+        head :no_content
+    end
+    private
+    def parameters
+        params.permit(:name,:email,:username,:status,:cohort)
     end
 end
