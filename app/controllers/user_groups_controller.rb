ce3bad55ce233dec4c63d198fb7f51d70d488d5d
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-10-19 12:02:00 +0300

    a commit of user_groups controller with added create, index, show actions

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,2 +1,22 @@
 class UserGroupsController < ApplicationController
+
+    def index
+        render json: UserGroup.all
+    end
+
+    def show
+        user_group = UserGroup.find_by(id: params[:id])
+        render json: user_group
+    end
+
+    def create
+        user_group = UserGroup.create(user_group_params)
+        user = user_group.user
+    end
+
+    private
+
+    def user_group_params
+        params.permit(:user_id, :group_id)
+    end
 end
