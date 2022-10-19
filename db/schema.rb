78a36a42aa79eb4667d6876fdc9f62084964f166
Paul <paul.kiplagt@student.moringaschool.com>
2022-10-19 22:45:51 +0300

    comments create

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_19_081823) do
+ActiveRecord::Schema.define(version: 2022_10_19_194017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
@@ -50,6 +60,8 @@ ActiveRecord::Schema.define(version: 2022_10_19_081823) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "user_groups", "groups"
   add_foreign_key "user_groups", "users"
