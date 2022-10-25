7c35eca8991be0f9205cc33c8c72e99c9e625cd5
Brandon Temba <brandontemba560@gmail.com>
2022-10-25 16:15:31 +0300

    Vision mission page almost done

---
+++ client/src/Components/Vmc.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const VMC = () => {
+    return (
+        <section className="section bg-c-light border-top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 mb-4 text-center">
+                            <h3 className="main-heading">Vision, Mission and Values</h3>
+                            <div className="underline mx-auto"></div>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <h6>Our Vision</h6>
+                            <p>
+                                <b>A world in which anyone can create their future </b> <br/>
+                                Our vision is one that strives to connect our African <br/>
+                                people to the future they envision for themselves.<br/>
+                                This is the goal in-front of us and it keeps us striving <br/>
+                                for more. Simply put…we will know we are succeeding when <br/>
+                                people working in tech in Africa have the same earning potential as their <br/>
+                                global counterparts possessing the same aptitude and mindset. <br/>
+                            </p>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <h6>Our Mission</h6>
+                            <p>
+                                Lorem ipsum sdhdhshdgsdgdg................
+                            </p>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <h6>Our Values</h6>
+                            <p>
+                                Lorem ipsum sdhdhshdgsdgdg................
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    );
+}
+
+export default VMC;
