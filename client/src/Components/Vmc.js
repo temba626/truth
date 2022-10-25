d4d876ea175bad3aac2435cf358ea3110500663c
Brandon Temba <brandontemba560@gmail.com>
2022-10-25 16:44:19 +0300

    Vision updated

--- client/src/Components/Vmc.js
+++ client/src/Components/Vmc.js
@@ -13,25 +13,37 @@ const VMC = () => {
                             <h6>Our Vision</h6>
                             <p>
                                 <b>A world in which anyone can create their future </b> <br/>
-                                Our vision is one that strives to connect our African <br/>
+                                Our vision is one that strives to connect our African<br/>
                                 people to the future they envision for themselves.<br/>
                                 This is the goal in-front of us and it keeps us striving <br/>
-                                for more. Simply put…we will know we are succeeding when <br/>
-                                people working in tech in Africa have the same earning potential as their <br/>
-                                global counterparts possessing the same aptitude and mindset. <br/>
+                                for more. Simply put…we will know we are succeeding <br/>
+                                when people working in tech in Africa have the same<br/>
+                                earning potential as their global counterparts possessing<br/>
+                                the same aptitude and mindset. <br/>
                             </p>
 
                         </div>
                         <div className="col-md-4 text-center">
                             <h6>Our Mission</h6>
                             <p>
-                                Lorem ipsum sdhdhshdgsdgdg................
+                            <b>To build talent and opportunities through transformative
+                                 tech-based learning experiences </b>
+
+                                Moringa School is a multi-disciplinary learning-accelerator
+                                committed to closing the skills-gap in Africa’s job markets
+                                by delivering transformative tech-based learning to
+                                high-potential jobseekers; and on graduation connecting
+                                them to local and international employers who desire
+                                high-quality tech talent
+
                             </p>
                         </div>
                         <div className="col-md-4 text-center">
                             <h6>Our Values</h6>
                             <p>
-                                Lorem ipsum sdhdhshdgsdgdg................
+                            These are the values that drive our operations and help to keep us on track to our core
+                            mandate to stakeholders.
+                            They speak to how we do what we do.
                             </p>
                         </div>
                     </div>
