3e11a889d2ec84c26d425e7d5111c51e56fdb527
Brian Wangora <brianwangora@gmail.com>
2022-11-03 09:20:49 +0000

    users route correction 1

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :messages
   resources :adverts
-  resources :users
+  # resources :users
 
    # Serve websocket cable requests in-process
    mount ActionCable.server => '/cable'
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
 
-  # get "/users", to: "users#index"
+  get "/users", to: "users#index"
+  put "/users/:id", to: "users#update"
 
 
   post "/login", to: "sessions#create"
